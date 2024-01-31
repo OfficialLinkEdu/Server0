7a52782e3ec7c91f2101b1fd831467f90334d21c
danielmc02 <digity63@gmail.com>
2024-01-31 12:16:12 -0800

    succesfully implemented handshake between servers

--- ApplicationServer/src/auth/auth_service.rs
+++ ApplicationServer/src/auth/auth_service.rs
@@ -1,21 +1,18 @@
 pub mod auth_service {
-    use std::os::macos::raw::stat;
-    use std::sync::Arc;
+    use std::{os::macos::raw::stat, sync::Arc};
 
-    use axum::body::Body;
-    use axum::extract::{Json, State};
+    use argon2::{
+        password_hash::{rand_core::OsRng, SaltString},
+        PasswordHasher,
+    };
+    use axum::{extract::{Json, State}, http::request};
     use axum::http::StatusCode;
-    use axum::response::{IntoResponse, Response};
-
-    use axum::routing::{get, post};
-    use axum::Router;
-    use rand::Rng;
-    use serde::Deserialize;
-    use sqlx::postgres::PgQueryResult;
-    use sqlx::prelude::FromRow;
-    use sqlx::query_as;
 
     use crate::AppState;
+    use axum::routing::{get, post};
+    use axum::Router;
+    use serde::Deserialize;
+    use sqlx::prelude::FromRow;
     #[derive(Debug, Deserialize, FromRow)]
     struct RegisterUserRequest {
         user_name: String,
@@ -24,18 +21,27 @@ pub mod auth_service {
         school_code: String,
     }
 
+    #[derive(sqlx::FromRow)]
+    struct PrivateUserInformation {
+        id: String,
+        password_hash: String,
+        salt: String,
+        user_name: String,
+    }
+
     //Used for registering a new user
     //#[debug_handler]
-    pub async fn register_user(
+    async fn register_user(
         State(state): State<AppState>,
         Json(payload): Json<RegisterUserRequest>,
     ) -> StatusCode {
         //Step1: Query if an existing user exists (email)
-        let result_query =
-            sqlx::query_as::<_, RegisterUserRequest>("SELECT FROM users WHERE email = $1")
-                .bind(&payload.email)
-                .fetch_one(&state.db_pool)
-                .await;
+        let result_query = sqlx::query_as::<_, PrivateUserInformation>(
+            "SELECT CAST(id AS Text), password_hash, salt, user_name FROM users WHERE email = $1",
+        )
+        .bind(&payload.email)
+        .fetch_one(&state.db_pool)
+        .await;
 
         match result_query {
             Ok(e) => {
@@ -46,19 +52,65 @@ pub mod auth_service {
                 println!("ERROR IS {}\n", e);
                 //Not found, register user
                 //step 1: hash password
-                let mut salt_array: [u8; 16] = [0; 16];
-                rand::thread_rng().fill(&mut salt_array);
-                println!("\nTHe salted password is {:?}\n", salt_array);
-                let hashed_password =
-                    bcrypt::hash_with_salt(payload.password, bcrypt::DEFAULT_COST, salt_array)
-                        .unwrap();
-                println!("\nThe Hashed password is {:?}", hashed_password);
-                // Step 2: insert new user into users table
-                let query_result =   sqlx::query("INSERT INTO users (email, password_hash, salt, user_name) VALUES($1, $2, $3, $4)").bind(payload.email).bind(hashed_password ).bind(salt_array).bind(payload.user_name).execute(&state.db_pool).await;
+                let mut salt_array: SaltString = SaltString::generate(OsRng);
+                println!("Salt array is: {}", salt_array);
+                let password_hash = argon2::Argon2::default()
+                    .hash_password(&payload.password.as_bytes(), &salt_array)
+                    .unwrap()
+                    .hash
+                    .unwrap()
+                    .to_string();
+                println!("\nHash is: {:?}", password_hash);
 
-                StatusCode::NOT_FOUND
+                // Step 2: insert new user into users table
+                let query_result =   sqlx::query("INSERT INTO users (email, password_hash, salt, user_name) VALUES($1, $2, $3, $4)").bind(&payload.email).bind(password_hash).bind(salt_array.to_string()).bind(payload.user_name).execute(&state.db_pool).await;
+                // on sucesfful register, creaete a request to the respective school url server to register users in
+         let res =    state.http_client.get("http://localhost:85/").send().await.unwrap();
+                println!("{:?}",res);
+
+
+
+                match query_result {
+                    Ok(result) => {
+                        let new_payload: LoginForm = LoginForm {
+                            email: payload.email,
+                            password: payload.password,
+                        };
+                        return sign_in_user(Json(new_payload), State(state)).await;
+                    }
+                    Err(_) => {
+                        return StatusCode::UNPROCESSABLE_ENTITY;
+                    }
+                }
             }
         }
+    }
+
+    #[derive(Debug, Deserialize)]
+
+    struct LoginForm {
+        email: String,
+        password: String,
+    }
+
+    async fn sign_in_user(
+        Json(payload): Json<LoginForm>,
+        State(state): State<AppState>,
+    ) -> StatusCode {
+        /*
+        Since this will be the only sign in,
+        re-query user information
+            */
+
+        let query_result: PrivateUserInformation = sqlx::query_as::<_, PrivateUserInformation>(
+            "SELECT CAST(id AS TEXT), password_hash, salt, user_name FROM users WHERE email = $1",
+        )
+        .bind(&payload.email)
+        .fetch_one(&state.db_pool)
+        .await
+        .unwrap();
+
+        StatusCode::OK
     }
 
     pub fn auth_routers() -> Router<AppState> {
--- ApplicationServer/src/main.rs
+++ ApplicationServer/src/main.rs
@@ -1,10 +1,11 @@
 use axum::{extract::State, routing::get, Router};
 mod auth;
-use auth::auth_service::auth_service::{auth_routers, register_user};
+use auth::auth_service::auth_service::auth_routers;
 
 mod email;
 use email::email_service::email_service::routers;
 
+use reqwest::Client;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 #[tokio::main]
@@ -12,15 +13,21 @@ async fn main() {
     // Init db pool
     let pg_pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://LINKEDU:123@central_user_database:5432/CentralUserDatabase")
+        .connect("postgres://LINKEDU:123@localhost:5432/CentralUserDatabase")
+        //      .connect("postgres://LINKEDU:123@central_user_database:5432/CentralUserDatabase")
         .await
         .unwrap();
+    let http_client = reqwest::Client::new();
 
-    let shared_state = AppState { db_pool: pg_pool };
+    let shared_state = AppState {
+        db_pool: pg_pool,
+        http_client: http_client,
+    };
 
     let app = Router::new()
-        .route("/", get(hello_world)) 
-        .nest("/authService", auth_routers()).with_state(shared_state)
+        .route("/", get(hello_world))
+        .nest("/authService", auth_routers())
+        .with_state(shared_state)
         .nest("/emailService", routers());
 
     let server = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
@@ -35,4 +42,5 @@ async fn hello_world(State(state): State<AppState>) -> &'static str {
 #[derive(Clone)]
 struct AppState {
     db_pool: Pool<Postgres>,
+    http_client: Client,
 }
